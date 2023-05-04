4f0ebbd3cec116cd16049691f0f51072cba8b509
Israel de Souza <apps.sites.sistematico@gmail.com>
2023-05-04 11:14:44 -0300

    iniciando primeira versão da calculadora

---
+++ index.js
@@ -0,0 +1,11 @@
+/*  1. Capturar elementos
+    2. Reconhecer números e sinais
+    3. Realizar operação escolhida
+    4. Exibir resultado na tela
+*/
+
+const somar = document.getElementById('somar');
+const subtrair = document.getElementById('subtrair');
+const dividir = document.getElementById('dividir');
+const multiplicar = document.getElementById('multiplicar');
+
