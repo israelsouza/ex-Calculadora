8786f2c392359cc13aea3922557532798c0cdbc3
Israel de Souza <apps.sites.sistematico@gmail.com>
2023-05-05 10:12:44 -0300

    v1 finalizada

--- index.js
+++ index.js
@@ -9,3 +9,45 @@ const subtrair = document.getElementById('subtrair');
 const dividir = document.getElementById('dividir');
 const multiplicar = document.getElementById('multiplicar');
 
+somar.addEventListener('click', () => {
+    n1 = prompt('Digite o primeiro valor');
+    if (n1 == '') return alert('digite um valor')
+
+    n2 = prompt('Digite o segundo valor');
+    if (n2 == '') return alert('digite um valor')
+
+    alert(`A soma entre ${n1} e ${n2} é igual a ${parseInt(n1) + parseInt(n2)}`);
+})
+
+subtrair.addEventListener('click', () => {
+    n1 = prompt('Digite um valor');
+    if (n1 < 0 || n1 == '') return alert('Digite um valor maior que 0')
+
+    n2 = prompt('Digite outro valor');
+    if (n2 == '') return alert('digite um valor')
+    
+    alert(`A subtração entre ${n1} e ${n2} é igual a ${parseInt(n1) - parseInt(n2)}`);
+})
+
+dividir.addEventListener('click', () => {
+    n1 = prompt('Digite o primeiro valor');
+    if (n1 < 1 || n1 == '') return alert('Digite um valor maior que 0')
+
+    n2 = prompt('Por quanto deseja dividir?');
+    if (n2 < 1 || n2 == '') return alert('Digite um valor maior que 0')
+
+    resultadoDivisao = parseFloat(n1) / parseFloat(n2);
+
+    alert(`A divisão entre ${n1} e ${n2} é igual a ${resultadoDivisao.toFixed(2)}`)
+})
+
+multiplicar.addEventListener('click', () =>{
+    n1 = prompt('Digite um valor maior que 0');
+    if ( n1<1 || n1=='' ){   return alert('Digite um valor maior que 0')   };
+
+    n2 = prompt('Digite outro valor maior que 0');
+    if ( n2<1 || n2=='' ){   return alert('Digite um valor maior que 0')   };
+    
+    resultadoMultiplicacao = parseInt(n1) * parseInt(n2);
+    alert(`O multiplicação entre ${n1} e ${n2} é igual a ${resultadoMultiplicacao}`);
+})
