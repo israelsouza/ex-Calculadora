ce0d0a32090a0b73562cd3eb76e4489319735a00
Israel de Souza <apps.sites.sistematico@gmail.com>
2023-05-05 11:09:24 -0300

    iniciando v2

--- index.js
+++ index.js
@@ -2,21 +2,26 @@
     2. Reconhecer números e sinais
     3. Realizar operação escolhida
     4. Exibir resultado na tela
+    CAIXA DE RESPOSTA PRECISA SER LIMPA APÓS SEGUNDO CLICK BOTAO 
+    FAZER TEXTO COM CSS DINAMICO, E RETIRAR createTextNode()
 */
 
 const somar = document.getElementById('somar');
 const subtrair = document.getElementById('subtrair');
 const dividir = document.getElementById('dividir');
 const multiplicar = document.getElementById('multiplicar');
+const resultado = document.getElementById('resultado');
 
 somar.addEventListener('click', () => {
+
     n1 = prompt('Digite o primeiro valor');
     if (n1 == '') return alert('digite um valor')
 
     n2 = prompt('Digite o segundo valor');
     if (n2 == '') return alert('digite um valor')
 
-    alert(`A soma entre ${n1} e ${n2} é igual a ${parseInt(n1) + parseInt(n2)}`);
+    texto = document.createTextNode(`A soma de ${n1} e ${n2} é igual a ${parseInt(n1) + parseInt(n2)}`);
+    resultado.appendChild(texto);
 })
 
 subtrair.addEventListener('click', () => {
@@ -26,7 +31,8 @@ subtrair.addEventListener('click', () => {
     n2 = prompt('Digite outro valor');
     if (n2 == '') return alert('digite um valor')
     
-    alert(`A subtração entre ${n1} e ${n2} é igual a ${parseInt(n1) - parseInt(n2)}`);
+    texto = document.createTextNode(`A subtração de ${n1} por ${n2} é igual a ${parseInt(n1) - parseInt(n2)}`);
+    resultado.appendChild(texto);
 })
 
 dividir.addEventListener('click', () => {
@@ -38,7 +44,8 @@ dividir.addEventListener('click', () => {
 
     resultadoDivisao = parseFloat(n1) / parseFloat(n2);
 
-    alert(`A divisão entre ${n1} e ${n2} é igual a ${resultadoDivisao.toFixed(2)}`)
+    texto = document.createTextNode(`A divisão entre ${n1} e ${n2} é igual a ${resultadoDivisao.toFixed(2)}`);
+    resultado.appendChild(texto);
 })
 
 multiplicar.addEventListener('click', () =>{
@@ -49,5 +56,7 @@ multiplicar.addEventListener('click', () =>{
     if ( n2<1 || n2=='' ){   return alert('Digite um valor maior que 0')   };
     
     resultadoMultiplicacao = parseInt(n1) * parseInt(n2);
-    alert(`O multiplicação entre ${n1} e ${n2} é igual a ${resultadoMultiplicacao}`);
+
+    texto = document.createTextNode(`A multiplicação de ${n1} por ${n2} é igual a ${resultadoMultiplicacao}`);
+    resultado.appendChild(texto);
 })
